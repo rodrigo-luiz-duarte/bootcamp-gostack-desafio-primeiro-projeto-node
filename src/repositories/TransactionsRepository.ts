f98d3bc64a6647fcbdfd766dc2715912cc66ba7b
Rodrigo Luiz Duarte <rodrigo.luizduarte@gmail.com>
2021-02-05 12:13:31 -0300

    Iniciado o desafio.

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions
+      .map(transaction => {
+        if (transaction.type === 'income') {
+          return transaction.value;
+        }
+        return 0;
+      })
+      .reduce((v1, v2) => {
+        return v1 + v2;
+      });
+
+    const outcome = this.transactions
+      .map(transaction => {
+        if (transaction.type === 'outcome') {
+          return transaction.value;
+        }
+        return 0;
+      })
+      .reduce((v1, v2) => {
+        return v1 + v2;
+      });
+
+    const total = income + outcome;
+
+    const balance = { income, outcome, total };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
